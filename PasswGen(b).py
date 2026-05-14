cde0426adb7d4ec88863b28c4346dde8665ce94c
dinekumar0025 <dineshkumar25msd7@gmail.com>
2026-05-14 08:49:28 +0530

    Password Generator in Python

--- PasswGen(b).py
+++ PasswGen(b).py
@@ -1,34 +1,30 @@
-# Password Generator in Python
+# Password Generator
 
 import random
 import string
 
 print("=== Random Password Generator ===")
 
-# Get password length from user
 length = int(input("Enter password length: "))
 
-# Character type options
 include_letters = input("Include letters? (yes/no): ").lower()
 include_numbers = input("Include numbers? (yes/no): ").lower()
 include_symbols = input("Include symbols? (yes/no): ").lower()
 
-# Store selected characters
 characters = ""
 
-# Add letters
+# Adding letters
 if include_letters == "yes":
     characters += string.ascii_letters   # a-z and A-Z
 
-# Add numbers
+# Adding numbers
 if include_numbers == "yes":
     characters += string.digits          # 0-9
 
-# Add symbols
+# Adding symbols
 if include_symbols == "yes":
     characters += string.punctuation     # !@#$%^&* etc.
 
-# Check if at least one option is selected
 if characters == "":
     print("Error: You must select at least one character type.")
 else:
